4a1a0c727ce4196837c63f13278b508cd473998c
payam-ranjbar <payam.ranjbar@live.com>
2018-07-09 12:52:08 +0430

    auth completed

--- vidido/settings.py
+++ vidido/settings.py
@@ -126,4 +126,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 LOGIN_REDIRECT_URL = 'test'
-LOGOUT_REDIRECT_URL = 'thanks'
+LOGOUT_REDIRECT_URL = '/accounts/login'
